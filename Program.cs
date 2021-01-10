aae74daa1eb6631c789303debce6e91493e43be3
Marcos Silvano <marcossilvano@gmail.com>
2021-01-09 21:27:58 -0300

    CMEAE

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes_Metodos_E_Atributos_Estaticos
+{
+    public static class Conversor
+    {
+
+        private static float CotacaoDolar = 5.24f;
+
+        public static float RealParaDolar(float valorRS){
+            return valorRS / CotacaoDolar;
+        }
+
+        public static float DolarParaReais(float valorUS){
+            return CotacaoDolar * valorUS;
+        }
+    }
+}
